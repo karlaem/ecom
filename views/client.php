4939d93b508cf8eda02ef65d310d927e0026c0a3
karlaem <60242444+karlaem@users.noreply.github.com>
2020-11-13 18:36:21 -0800

    updated css

--- views/client.php
+++ views/client.php
@@ -3,11 +3,13 @@
     if(isset($this->oClient)){
         foreach ($this->oClient as $client){
         ?>
+        <div class="edithProfilePhoto">
+				<div id="thePhoto" imgSrc="<?=$client->image?>" ></div>
+		</div>
         <h2><?=$client->first_name?> <?=$client->last_name?></hw>
         <p><?=$client->email?></p>
         <p><?=$client->age?></p>
         <p><?=$client->country?></p>
-        <p><?=$client->image?></p>
         <?php
         }
     }
--- views/cms.php
+++ views/cms.php
@@ -6,40 +6,12 @@
                 <h1>Welcome <?=$this->oCurUser->username?></h1>
                 <div class="clientour">
                     <a href="index.php?controller=user&action=clients">Our Clients</a>
+                    <a href="index.php?controller=user&action=products">Our Products</a>
                 </div><!--clientour-->
             </div><!--.left-->
-            <div class="clientnames">       
-                <div class="clientmenu">
-                    <div class="clientnamelist">
-                    <?php
-                    if(isset($this->oClients)){
-                    foreach ($this->oClients as $client){
-                    ?>
-                    <a href="index.php?controller=user&action=clients&clientid=<?=$client->id?>"><?=$client->first_name?></a>
-                    <?php
-                    }
-                    }
-                    ?>
-                    </div><!--.clientnamelist-->
-                    <div class="clientinfo">
-                    <?php
-                    if(isset($this->oClient)){
-                    foreach ($this->oClient as $client){
-                    ?>
-                     <div class="edithProfilePhoto">
-                        <div id="thePhoto" imgSrc="<?=$client->image?>" ></div>
-                    </div>
-                    <h2><?=$client->first_name?> <?=$client->last_name?></hw>
-                    <p><?=$client->email?></p>
-                    <p><?=$client->age?></p>
-                    <p><?=$client->country?></p>
-                    <?php
-                    }
-                    }
-                    ?>
-                    </div><!--.clientinfo-->
-                </div><!--clientmenu-->
-            </div><!--clientnames-->
+            <div class="clientright">
+                <?=$this->clientlist?>
+            </div>
         </div><!--cmsmenu-->
     </div><!--.margins-->
 </div><!--.hero2-->
