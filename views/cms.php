1e382e4ac90f48c3a4a87a1bd963ff86deb3323f
karlaem <60242444+karlaem@users.noreply.github.com>
2020-11-13 17:07:04 -0800

    css edited

--- views/cms.php
+++ views/cms.php
@@ -1,15 +1,44 @@
+<div class="hero2">
 <?=$this->header?><!--load header-->
-<div class="margins">
-    <div class="cmsmenu">
-    <div class="left"> 
-        <h1>Welcome <?=$this->oCurUser->username?></h1>
-        <div class="clientour">
-            <a href="index.php?controller=user&action=clients">Our Clients</a>
-        </div>
-    </div>
-    <div class="clientnames">
-        <?=$this->clientlist?>
-    </div>
-    </div>
-</div>
+    <div class="margins">
+        <div class="cmsmenu">
+            <div class="left"> 
+                <h1>Welcome <?=$this->oCurUser->username?></h1>
+                <div class="clientour">
+                    <a href="index.php?controller=user&action=clients">Our Clients</a>
+                </div><!--clientour-->
+            </div><!--.left-->
+            <div class="clientnames">       
+                <div class="clientmenu">
+                    <div class="clientnamelist">
+                    <?php
+                    if(isset($this->oClients)){
+                    foreach ($this->oClients as $client){
+                    ?>
+                    <a href="index.php?controller=user&action=clients&clientid=<?=$client->id?>"><?=$client->first_name?></a>
+                    <?php
+                    }
+                    }
+                    ?>
+                    </div><!--.clientnamelist-->
+                    <div class="clientinfo">
+                    <?php
+                    if(isset($this->oClient)){
+                    foreach ($this->oClient as $client){
+                    ?>
+                    <h2><?=$client->first_name?> <?=$client->last_name?></hw>
+                    <p><?=$client->email?></p>
+                    <p><?=$client->age?></p>
+                    <p><?=$client->country?></p>
+                    <p><?=$client->image?></p>
+                    <?php
+                    }
+                    }
+                    ?>
+                    </div><!--.clientinfo-->
+                </div><!--clientmenu-->
+            </div><!--clientnames-->
+        </div><!--cmsmenu-->
+    </div><!--.margins-->
+</div><!--.hero2-->
 <!--End of cms-->
